828f61d7d337d573c4b72ca238e3e44ef9b5813b
Sandra Soza <ssozazambrano@luc.edu>
2026-02-16 18:44:14 +0000

    name & course

--- PA2/BinaryTree.java
+++ PA2/BinaryTree.java
@@ -1,5 +1,5 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Sandra Soza Zambrano / 272 -002 ***
  *
  * This file shows a simple Binary Tree (we'll insert like a BST for convenience).
  * You will implement the following methods:
