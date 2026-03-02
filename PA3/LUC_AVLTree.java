21402ac22e6ef1a232f9d835cae5e2a8688bbebc
Sandra Soza <ssozazambrano@luc.edu>
2026-03-02 05:10:17 +0000

    added code

--- PA3/LUC_AVLTree.java
+++ PA3/LUC_AVLTree.java
@@ -165,3 +165,65 @@ public class LUC_AVLTree {
         return c;
     }
 }
+
+import java.util.*;
+
+public class RBTreeProblems {
+
+  /**
+   * closestValue()
+   *
+   * Given a TreeSet of integers and a target value, return the element
+   * in the set that is numerically closest to the target.
+   *
+   * If two values are equally close, return the smaller value.
+   *
+   * You must use TreeSet navigation methods.
+   */
+  public static int closestValue(TreeSet<Integer> set, int target) {
+    // TODO
+    return 0;
+  }
+
+  /**
+   * kClosestValues()
+   *
+   * Return the k values in the set that are closest to the target value.
+   * The result must be returned as a TreeSet so it is sorted ascending.
+   *
+   * Suggested strategy:
+   *   - Start near the target using floor() and/or ceiling().
+   *   - Expand outward using lower() and higher().
+   */
+  public static Set<Integer> kClosestValues(TreeSet<Integer> set, int target, int k) {
+    // TODO
+    return new TreeSet<>();
+  }
+
+  /**
+   * rangeCount()
+   *
+   * Given a TreeMap<Integer,String> and a range [low, high] inclusive,
+   * return how many keys fall inside that range.
+   *
+   * You MUST use subMap() to obtain the range view of the map.
+   * Do NOT iterate over the entire map.
+   */
+  public static int rangeCount(TreeMap<Integer,String> map, int low, int high) {
+    // TODO
+    return 0;
+  }
+
+  /**
+   * longestGap()
+   *
+   * Given a TreeSet of integers representing sorted positions,
+   * return the largest difference between consecutive values.
+   *
+   * If the set has fewer than 2 elements, return 0.
+   */
+  public static int longestGap(TreeSet<Integer> set) {
+    // TODO
+    return 0;
+  }
+}
